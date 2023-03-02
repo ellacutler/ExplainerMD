17d51f25d93d30c72273482d8dcaf86de9a60846
Marko Veljanovski <markoveljanovski2025@u.northwestern.edu>
2023-03-01 22:10:32 -0600

    Merge 2 models, add ner pipeline

--- ML/md-NER.py
+++ ML/md-NER.py
@@ -25,8 +25,12 @@ for text, annot in tqdm(train['annotations']):
 db.to_disk('./training_data.spacy')
 
 ''' Get best model'''
-nlp_ner = spacy.load("./model-best")
+custom_ner = spacy.load("./model-best")
 
-doc = nlp_ner('''Bob should take 2 pills of Metformin orally every evening''')
+bc5cdr_ner = spacy.load("en_ner_bc5cdr_md")
+text = "Northwestern M Medicine NMG Pharmacy Rx #: 7001996 Ryan Newkirk Take 1 tablet by mouth 2 days. penicillin VK 500 Generic for: VEETID Lori A. Schwarcz, MD NDC: 57237-041-99 RISING PHARM Exp: 7/31/2023 CAUTION Federal law prohibits the transfer of this drug to any pes"
+
+bc5cdr_ner.add_pipe("ner", name="custom", source=custom_ner)
+doc = bc5cdr_ner(text)
 for ent in doc.ents:
   print(ent.text,ent.label_)
